d52387a4a63586a7b1c631177874216e66f41782
WrightCD <125230177+WrightCD@users.noreply.github.com>
2025-06-06 21:14:56 -0400

    Visual cards and alternating turns added

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 #Game Scores will make more dynamic with betting later on
 player_score = 0
 cpu_score = 0
+current_round = 0
 
-def generate_hands():
-    p1_hand = ['S'] + ['C']*4
-    cpu_hand = ['K'] + ['C'] * 4
+def generate_hands(round_number):
+    slave_hand = ['S'] + ['C']*4
+    king_hand = ['K'] + ['C'] * 4
+
+    if round_number % 2 == 0:
+        p1_hand = slave_hand
+        cpu_hand = king_hand
+    else:
+        p1_hand = king_hand
+        cpu_hand = slave_hand
 
     return p1_hand, cpu_hand
 
@@ -19,6 +27,9 @@ def determine_winner(p1_card, cpu_card):
         ('S', 'C'): 'cpu',
         ('C', 'K'): 'cpu',
         ('C', 'C'): 'draw',
+        ('C', 'S'): 'player',
+        ('K', 'C'): 'player',
+        ('K', 'S'): 'cpu',
     }
     return rules.get((p1_card, cpu_card), 'draw')
 
@@ -33,15 +44,17 @@ def index():  # put application's code here
 
 @app.route('/start', methods=['POST'])
 def start_game():
-    global player_score, cpu_score, hands
-    player_score = 0
-    cpu_score = 0
-    p1_hand, cpu_hand = generate_hands()
+    global hands, current_round, player_score, cpu_score
+    p1_hand, cpu_hand = generate_hands(current_round)
+    current_round += 1
     hands['player'] = p1_hand
     hands['cpu'] = cpu_hand
     return jsonify({
-        'p1_hand': p1_hand
+        'p1_hand': p1_hand,
+        'player_score': player_score,
+        'cpu_score': cpu_score
     })
+
 
 @app.route('/play', methods=['POST'])
 def play_turn():
@@ -69,7 +82,7 @@ def play_turn():
             cpu_score += 1
 
         # Reset hands for new round
-        new_p1_hand, new_cpu_hand = generate_hands()
+        new_p1_hand, new_cpu_hand = generate_hands(current_round)
         hands['player'] = new_p1_hand
         hands['cpu'] = new_cpu_hand
         reset = True

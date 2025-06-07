fbbbc7e5a019ce025c526d6097d66b0e465ff599
WrightCD <carldonovanwright@gmail.com>
2025-06-07 11:39:03 -0400

    Betting logic, background image, wallet system

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ import random
 
 app = Flask(__name__)
 
-#Game Scores will make more dynamic with betting later on
 player_score = 0
 cpu_score = 0
 current_round = 0
+wallet = 1000.00
+current_bet = 0.00
+
+EMPEROR_MULTIPLIER = 0.5
+SLAVE_MULTIPLIER = 2.0
+
+hands = {
+    'player': [],
+    'cpu': []
+}
+
 
 def generate_hands(round_number):
-    slave_hand = ['S'] + ['C']*4
+    slave_hand = ['S'] + ['C'] * 4
     king_hand = ['K'] + ['C'] * 4
+    return (slave_hand, king_hand) if round_number % 2 == 0 else (king_hand, slave_hand)
 
-    if round_number % 2 == 0:
-        p1_hand = slave_hand
-        cpu_hand = king_hand
-    else:
-        p1_hand = king_hand
-        cpu_hand = slave_hand
-
-    return p1_hand, cpu_hand
 
 def determine_winner(p1_card, cpu_card):
     rules = {
@@ -33,61 +36,77 @@ def determine_winner(p1_card, cpu_card):
     }
     return rules.get((p1_card, cpu_card), 'draw')
 
-hands = {
-    'player': [],
-    'cpu': []
-}
 
 @app.route('/')
-def index():  # put application's code here
+def index():
     return render_template("index.html")
+
 
 @app.route('/start', methods=['POST'])
 def start_game():
-    global hands, current_round, player_score, cpu_score
+    global hands, current_round, current_bet
+
+    if current_round == 0:
+        current_bet = 100.00
+    else:
+        data = request.get_json()
+        if data is None or 'bet' not in data:
+            return jsonify({'error': 'Bet amount is required.'}), 400
+        try:
+            bet = round(float(data['bet']), 2)
+        except (TypeError, ValueError):
+            return jsonify({'error': 'Invalid bet value.'}), 400
+        if bet > wallet:
+            return jsonify({'error': 'Insufficient funds for this bet.'}), 400
+        current_bet = bet
+
     p1_hand, cpu_hand = generate_hands(current_round)
-    current_round += 1
     hands['player'] = p1_hand
     hands['cpu'] = cpu_hand
+    current_round += 1
+
     return jsonify({
         'p1_hand': p1_hand,
         'player_score': player_score,
-        'cpu_score': cpu_score
+        'cpu_score': cpu_score,
+        'wallet': round(wallet, 2)
     })
 
 
 @app.route('/play', methods=['POST'])
 def play_turn():
-    global player_score, cpu_score, hands
+    global player_score, cpu_score, hands, wallet, current_bet
 
     data = request.get_json()
-    selected_card = data['card']
+    selected_card = data.get('card')
 
     if selected_card not in hands['player']:
         return jsonify({'error': 'Invalid card'}), 400
 
     hands['player'].remove(selected_card)
-
     cpu_card = random.choice(hands['cpu'])
     hands['cpu'].remove(cpu_card)
 
     winner = determine_winner(selected_card, cpu_card)
-
     round_ended = selected_card in ['S', 'K'] or cpu_card in ['S', 'K']
 
     if round_ended:
         if winner == 'player':
             player_score += 1
+            # Correct wallet update when player wins:
+            if selected_card == 'K':
+                wallet += round(current_bet * EMPEROR_MULTIPLIER, 2)
+            elif selected_card == 'S':
+                wallet += round(current_bet * SLAVE_MULTIPLIER, 2)
+            elif selected_card == 'C':
+                wallet += round(current_bet * EMPEROR_MULTIPLIER, 2)
         elif winner == 'cpu':
+            wallet -= round(current_bet, 2)
             cpu_score += 1
 
-        # Reset hands for new round
         new_p1_hand, new_cpu_hand = generate_hands(current_round)
         hands['player'] = new_p1_hand
         hands['cpu'] = new_cpu_hand
-        reset = True
-    else:
-        reset = False
 
     return jsonify({
         'p1_card': selected_card,
@@ -95,9 +114,23 @@ def play_turn():
         'winner': winner,
         'player_score': player_score,
         'cpu_score': cpu_score,
+        'wallet': round(wallet, 2),
         'remaining_player_hand': hands['player'],
-        'reset': reset
+        'reset': round_ended,
     })
+
+@app.route('/reset', methods=['POST'])
+def reset_game():
+    global player_score, cpu_score, current_round, wallet, current_bet, hands
+
+    player_score = 0
+    cpu_score = 0
+    current_round = 0
+    wallet = 1000.00
+    current_bet = 0.00
+    hands = {'player': [], 'cpu': []}
+
+    return jsonify({'wallet': round(wallet, 2)})
 
 
 if __name__ == '__main__':
